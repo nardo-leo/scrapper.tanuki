b3cf71cfff1af52a925e0d4b6dbd04298e6353a5
nardo-leo <jora.karuzo@gmail.com>
2020-10-01 19:38:17 +0300

    Parse all goods with name, price, saleand pic

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# scrapper.tanuki
-
+Parse all goods with name, price, 10% sale and pics from https://www.tanuki.ru/menu/
--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 
-# TODO wrap in function
-# def scrap():
+# format float numbers
+def format_float(num):
+    if num % 1 == 0:
+        return int(num)
+    else:
+        return num
+
+
 # result list
 goods = []
 
-# read https://www.tanuki.ru/menu/
+# read url
 url = 'https://www.tanuki.ru/menu/'
 # change headers to avoid blocking
 headers = {
@@ -24,11 +30,21 @@ soup = BeautifulSoup(text, features='lxml')
 catalog = soup.find('div', {'class': 'catalog'})
 product_items = catalog.find_all('div', {'class': 'product'})
 
+# number for img naming
+num = 0
+
 for product_item in product_items:
     name = product_item.find('div', {'class': 'product__title'}).text
-    price = product_item.find('div', {'class': 'product__price'}).text
+    price = product_item.find('div', {'class': 'product__price'}).text[0:-4]
+    sale_price = format_float(int(price) * 0.9)
     pic = product_item.find('img', {'class': 'product__image'})['src']
-    item = [name, price, pic]
-    goods.append(item)
 
-print(goods)
+    num += 1
+
+    # write to csv
+    with open('tanuki_dataset.csv', 'a', encoding='utf-8') as f:
+        f.write(f'{name},{price} ₽,{sale_price} ₽,{num}.jpg\n')
+
+    # save pics from links
+    with open(f'{num}.jpg', 'wb') as f:
+        f.write(requests.get(pic).content)
